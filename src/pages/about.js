4e7267fdb0e4b0a5885bf699c7ec47f08d6f3cf9
CheoR <cheo.roman+github@gmail.com>
2021-12-20 22:14:29 -0600

    build: add pages.

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      I am Export Default about page.
+    </div>
+  )
+}
+
+export default About
---
+++ src/pages/contact.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Contact() {
+  return (
+    <div>
+      Function declaration contact page. 
+    </div>
+  )
+}
+
+// import React, { Component } from 'react'
+
+// export default class Contact extends Component {
+//   render() {
+//     return (
+//       <div>
+//         Class Component Contact Page
+//       </div>
+//     )
+//   }
+// }

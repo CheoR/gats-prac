709f1ad3da07db7bf5cf2daa928cdc672f5d2dc2
CheoR <cheo.roman+github@gmail.com>
2021-12-21 15:21:28 -0600

    css: module css example.

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import WrapperLayout from '../components/WrapperLayout'
+import { page, text } from "../examples/about.module.css"
 
 const About = () => {
   return (
     <WrapperLayout>
-      <h2>I am Export Default about page.</h2>
+      <div className={page}>
+        <h2>I am Export Default about page.</h2>
+        <h2>Another h2</h2>
+        <p className={text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur adipisci aut vitae illo ab esse quo inventore eius cumque corporis?
+        </p>
+      </div>
     </WrapperLayout>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,26 @@
 import React from "react"
 import { Link } from "gatsby"
 import WrapperLayout from "../components/WrapperLayout"
+import { page, text } from "../examples/home.module.css"
 
-const headerStyle = {
-  color: "red",
-  textTransform: "capitalize"
-}
+
+// const headerStyle = {
+//   color: "red",
+//   textTransform: "capitalize"
+// }
 
 export default function Home() {
   return (
     <WrapperLayout>
-      <h1 style={headerStyle}>Home Page: I now have links Hello World</h1>
+      <div className={page}>
+        <h1>H1 Home Page</h1>
+        <h1>H1 Again</h1>
+        <h1>H1 Another</h1>
+        <p className={text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus numquam fugiat repudiandae, officiis illo ut! Modi eveniet, consequuntur ipsa beatae natus animi error quo ab magnam maxime odio sit iste totam minima architecto cum unde. Voluptatum delectus adipisci earum quasi vero doloremque illo enim incidunt, voluptas ratione corporis eveniet maiores?
+        </p>
+      </div>
+      {/* <h1 style={headerStyle}>Home Page: I now have links Hello World</h1>
       <h1>Also H1 tag</h1>
       <h1>Another H1 Tag</h1>
       <div>
@@ -29,7 +39,7 @@ export default function Home() {
         <a href="http://www.gatsbyjs.com/docs/how-to">
           a-tag External Link to how to docs
         </a>
-      </div>
+      </div> */}
     </WrapperLayout>
 
   )

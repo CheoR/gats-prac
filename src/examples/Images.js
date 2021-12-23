0821f703202a73f76dbc1b58bc84538b901b78c8
CheoR <cheo.roman+github@gmail.com>
2021-12-23 08:19:30 -0600

    build: static image constrained example.

--- src/examples/Images.js
+++ src/examples/Images.js
@@ -3,27 +3,72 @@ import React from 'react'
 import styled from 'styled-components'
 import { StaticImage } from 'gatsby-plugin-image'
 
-import big from '../assets/images/big.jpg'
 
 const Images = () => {
   return (
     <Wrapper>
-      <StaticImage
-        src="../assets/images/big.jpg"
-        alt="big food image"
-      />
-      <StaticImage
-        src="https://raw.githubusercontent.com/john-smilga/gatsby-v3-tutorial-recipes/main/src/assets/images/about.jpeg"
-        alt="remote hosted about image"
-      />
-      <h2>Gatsby Image</h2>
+      <article>
+        <h4>constrained/default</h4>
+        <StaticImage
+          src="../assets/images/recipe-1.jpeg"
+          alt="food"
+          // height={100}
+          placeholder="tracedSVG"
+          layout="constrained"
+          className="example-img"
+          as="section"
+        />
+      </article>
+      <article>
+        <h4>fixed</h4>
+        <StaticImage
+          src="../assets/images/recipe-1.jpeg"
+          alt="food"
+          // height={100}
+          placeholder="blurred"
+          layout="fixed"
+          width={200}
+          className="example-img"
+          as="div"
+        />
+      </article>
+            <article>
+        <h4>full width</h4>
+        <StaticImage
+          src="../assets/images/recipe-1.jpeg"
+          alt="food"
+          // height={100}
+          placeholder="dominantColor"
+          layout="fullWidth"
+          className="example-img"
+          as="article"
+        />
+      </article>
     </Wrapper>
   )
 }
 
 const Wrapper = styled.section`
-  img {
-    width: 200px;
+  width: 70vw;
+  margin: 0 auto;
+  display: grid;
+  text-align: center;
+  
+  article {
+    border: 2px dashed black;
+  }
+
+  .example-img {
+    border-radius: 1rem;
+    height: 300px;
+  }
+
+  @media (min-width: 992px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+
+  .example-img {
+    height: 200px;
   }
 `
 

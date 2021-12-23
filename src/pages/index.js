22610b25600d4afd4d924f2f461274ff9a81ec15
CheoR <cheo.roman+github@gmail.com>
2021-12-22 18:27:14 -0600

    build: large image being downloaded example

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,48 +1,13 @@
 import React from "react"
 import { Link } from "gatsby"
 import WrapperLayout from "../components/WrapperLayout"
-// import { page, text } from "../examples/home.module.css"
-// import { ExampleButton } from "../examples/button"
-
-// const headerStyle = {
-//   color: "red",
-//   textTransform: "capitalize"
-// }
+import Images from "../examples/Images"
 
 export default function Home() {
   return (
     <WrapperLayout>
       <h1>Home Page</h1>
-      {/* <div className={page}>
-        <ExampleButton>I am styled button </ExampleButton>
-        <h1>H1 Home Page</h1>
-        <h1>H1 Again</h1>
-        <h1>H1 Another</h1>
-        <p className={text}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus numquam fugiat repudiandae, officiis illo ut! Modi eveniet, consequuntur ipsa beatae natus animi error quo ab magnam maxime odio sit iste totam minima architecto cum unde. Voluptatum delectus adipisci earum quasi vero doloremque illo enim incidunt, voluptas ratione corporis eveniet maiores?
-        </p>
-      </div>
-      <h1 style={headerStyle}>Home Page: I now have links Hello World</h1>
-      <h1>Also H1 tag</h1>
-      <h1>Another H1 Tag</h1>
-      <div>
-        <Link to="/about">About</Link>
-      </div>
-      <div>
-        <Link to="/company/history">History</Link>
-      </div>
-      <div>
-        <Link to="/history">Broken company history link</Link>
-      </div>
-      <div>
-        <a href="/about">a-tag internal link to about</a>
-      </div>
-      <div>
-        <a href="http://www.gatsbyjs.com/docs/how-to">
-          a-tag External Link to how to docs
-        </a>
-      </div> */}
+      <Images />
     </WrapperLayout>
-
   )
 }

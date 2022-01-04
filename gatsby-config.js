7268bce444139db98b359217f5ab6c9535c7215d
CheoR <cheo.roman+github@gmail.com>
2022-01-04 12:41:47 -0600

    doc: query metadata example

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,31 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: "Simple Recepies",
+    description: "Simple Recepies with Gatsby",
+    author: "@CheoR",
+    person: {
+      name: "Peggy Pug",
+      age: 2,
+    },
+    simpledata: [
+      "item 1",
+      "item 2",
+      "item 3",
+      "item 4",
+      "item 5"
+    ],
+    complexData: [
+      {
+      name: "Rocky Rott",
+      age: 5,
+    },{
+      name: "Bully Bull",
+      age: 7,
+    },
+    ]
+  },
   plugins: [
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-image`,

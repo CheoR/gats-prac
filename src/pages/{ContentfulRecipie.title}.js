9e151a8e978e6497998853b1e583a4e5698687a1
CheoR <cheo.roman+github@gmail.com>
2022-01-11 11:38:35 -0600

    build: setup pages with filesystem route api. Pass property value as variable and use queries using variable name.

--- src/pages/{ContentfulRecipie.title}.js
+++ src/pages/{ContentfulRecipie.title}.js
@@ -1,23 +1,140 @@
 import React from 'react'
-import { graphql } from 'gatsby'
+import { Link, graphql } from 'gatsby'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
+import { BsClockHistory, BsClock, BsPeople } from 'react-icons/bs'
 
-const RecepieTemplate = (props) => {
+import WrapperLayout from '../components/WrapperLayout'
+
+const RecepieTemplate = ({ data }) => {
+  const {
+    title,
+    cookTime,
+    content,
+    prepTime,
+    servings,
+    description: { description },
+    image
+  } = data.contentfulRecipie
+
+  const { tags, instructions, ingredients, tools } = content
+
+  const imagePath = getImage(image)
+
   return (
-    <div>
-      <h4>Recepie: {props.params.title}</h4>
-    </div>
+    <WrapperLayout>
+      <main className="page">
+        <div className="recipe-page">
+          <section className="recipe-hero">
+            <GatsbyImage
+              image={imagePath}
+              alt={title}
+              className='about-img'
+            />
+            <article className="recipe-info">
+              <h2>{title}</h2>
+              <p>{description}</p>
+              <div className="recipe-icons">
+                <article>
+                  <BsClock />
+                  <h5>prep time</h5>
+                  <p>{prepTime} min.</p>
+                </article>
+                <article>
+                  <BsClockHistory />
+                  <h5>cook time</h5>
+                  <p>{cookTime} min.</p>
+                </article>
+                <article>
+                  <BsPeople />
+                  <h5>servings</h5>
+                  <p>{servings}</p>
+                </article>
+              </div>
+              <p className="recipe-tags">
+                Tags: 
+                {
+                  tags.map((tag, idx) => {
+                    return (
+                      <Link to={`/${tag}`} key={idx} >
+                        {tag}
+                      </Link>
+                    )
+                  })
+                }
+              </p>
+            </article> 
+          </section>
+          <section className="recipe-content">
+            <article>
+              <h4>instructions</h4>
+              {
+                ingredients.map((item, idx) => {
+                  return (
+                    <div key={idx} className="single-instruction">
+                      <header>
+                        <p>step {idx + 1} </p>
+                        <div></div>
+                      </header>
+                      <p>{item}</p>
+                    </div>
+                  )
+                })
+              }
+            </article>
+            <article className="second-column">
+              <div>
+                <h4>ingredients</h4>
+                {
+                  ingredients.map((item, idx) => {
+                    return (
+                      <p key={idx} className="single-ingredient">
+                        {item}
+                      </p>
+                    )
+                  })
+                }
+              </div>
+              <div>
+                <h4>tools</h4>
+                {
+                  tools.map((item, idx) => {
+                    return (
+                      <p key={idx} className="single-tools">
+                        {item}
+                      </p>
+                    )
+                  })
+                }
+              </div>
+            </article>
+          </section>
+        </div>
+      </main>
+    </WrapperLayout>
   )
 }
 
 export const query = graphql`
-  query getSingleRecepie($title:String) {
-    allContentfulRecipie(filter: {title: {eq: $title}}) {
-      nodes {
-        title
-        cookTime
+  query getSingleRecipe($title: String) {
+    contentfulRecipie(title: {eq: $title}) {
+      title
+      cookTime
+      prepTime
+      content {
+        tags
+        ingredients
+        instructions
+        tools
+      }
+      description {
+        description
+      }
+      servings
+      image {
+        gatsbyImageData(layout: CONSTRAINED, placeholder: TRACED_SVG)
       }
     }
-}
+  }
 `
 
 export default RecepieTemplate

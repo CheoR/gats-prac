bb27e2dd6d87fa9ef4db60b79d3a6d45c9cde899
CheoR <cheo.roman+github@gmail.com>
2022-01-10 22:59:36 -0600

    build: tags page example.

--- src/pages/tags.js
+++ src/pages/tags.js
@@ -1,12 +1,47 @@
 import React from 'react'
+import { graphql, Link } from 'gatsby'
 import WrapperLayout from '../components/WrapperLayout'
+import setupTags from '../utils/setupTags'
 
-const Tags = () => {
+
+const Tags = ({ data }) => {
+  console.log('in tags')
+  console.log(data)
+  console.log('---------')
+  const newTags = setupTags(data.allContentfulRecipie.nodes)
+
   return (
     <WrapperLayout>
-      <h1>Tags Page</h1>
+      <main className="page">
+        <section className="tags-page">
+          {
+            newTags.map((tag, idx) => {
+              const [text,value] = tag
+              return (
+                <Link to={`/${text}`} key={idx} className="tag">
+                  <h5>{text}</h5>
+                  <p>
+                    {value} recipe
+                  </p>
+                </Link>
+              )
+            })
+          }
+        </section>
+      </main>
     </WrapperLayout>
   )
 }
 
+export const query = graphql`
+  {
+    allContentfulRecipie {
+      nodes {
+        content {
+          tags
+        }
+      }
+    }
+  }
+`
 export default Tags
